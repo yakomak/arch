57a436481ef70652e60326eaa9fb2789dacea84a
Maxim <yakomak137@gmail.com>
2022-03-15 16:03:18 +0300

    Update ar1.py

--- ar1.py
+++ ar1.py
@@ -23,7 +23,25 @@ for i in fib():
 #
 
 #-2-----------------------------------------------
-
+music/  Top-level package
+  __init__.py      Initialize the music package
+ formats/ Subpackage for file conversions
+ __init__.py
+ wavread.py
+ wavwrite.py
+ aiffread.py
+ aiffwrite.py
+ auread.py
+ auwrite.py
+ ...
+ effects/ Subpackage for sound effects
+ __init__.py
+ echo.py
+ surround.py
+ reverse.py
+ ...
+ filters/ Subpackage for filters __init__.py equalizer.py vocoder.py karaoke.py ...
+Источник: https://pythonim.ru/osnovy/package-python
 #-3-----------------------------------------------
 
 #-4-----------------------------------------------
